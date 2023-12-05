a355466d9feb7ddf4d17c47f3fc2175c9a4fb8bb
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-12-05 22:44:56 +0530

    microservice files added

---
+++ eurekaServer/eurekaServer/src/test/java/com/eurekaserver/pkg/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
