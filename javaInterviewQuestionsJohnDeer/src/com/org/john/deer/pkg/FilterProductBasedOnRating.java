a9312848c8528e6e634caf35e0375c9044b28c71
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-12-24 07:30:08 +0530

    changes for technical interview questions

---
+++ javaInterviewQuestionsJohnDeer/src/com/org/john/deer/pkg/FilterProductBasedOnRating.java
@@ -0,0 +1,15 @@
+package com.org.john.deer.pkg;
+
+public class FilterProductBasedOnRating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//		productList.forEach(product -> {
+		//			product.getRatingHistory().stream().filter(item -> item.getRating() > 4).forEach(rating -> {
+		//				System.out("product" + product + " rating " + rating);
+		//			});
+		//		});
+
+	}
+
+}
